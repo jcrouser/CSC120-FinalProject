ca5eed1e11ebefade4995f2a19a4097039a57e99
Laisha Koon Perez <lkoonperez@smith.edu>
2022-12-01 10:23:22 -0500

    new file:   Bedroom.java 	new file:   Office.java 	modified:   Rooms.java

--- Rooms.java
+++ Rooms.java
@@ -1,3 +1,11 @@
 public class Rooms {
-    
+    private String name;
+    private String objects;
+    private String description;
+
+    public Rooms(String name, String description, String objects) {
+        this.name = name;
+        this.description = description;
+        this.objects = objects;
+    }
 }
