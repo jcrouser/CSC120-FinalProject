45989a2b05c765652f691232eba2dfca05883f2a
lzguo25 <lguo@smith.edu>
2022-11-30 23:18:04 -0500

    skeleton of HauntedHouse

--- HauntedHouse.java
+++ HauntedHouse.java
@@ -1,3 +1,51 @@
 public class HauntedHouse{
-    
+    private String address;
+    private String name;
+    private int numRooms;
+    private String description;
+
+
+    public HauntedHouse(String name, String address, int numRooms, String description){
+        this.name = name;
+        this.address= address;
+        this.numRooms = numRooms;
+        this.description = description;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public int numRooms(){
+        return this.numRooms;
+    }
+
+    public String description(){
+        return this.description;
+    }
+
+    /*Navigation Methods? */
+
+    public HauntedHouse enter(){
+        System.out.println("You've entered " + this.name + " located at " + this.address);
+        return this;
+    }
+
+   
+
+    public void showOptions(){
+        System.out.println("");
+    }
+
+    public String toString(){
+        return (this.name + "is a haunted house located at " + this.address + ".");
+    }
+    public static void main(String args[]){
+
+
+    }
 }
