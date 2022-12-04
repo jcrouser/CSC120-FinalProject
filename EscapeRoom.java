16ee6606d5bb3b9c7f4397b22156ef7eb368bc69
Laisha Koon Perez <lkoonperez@smith.edu>
2022-12-04 13:35:58 -0500

    modified:   EscapeRoom.java 	modified:   Rooms.java

--- EscapeRoom.java
+++ EscapeRoom.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class EscapeRoom {
     public static void main(String[] args) {
-        //Greeting for the game
-        System.out.println("Welcome to the Escape Room!");
-        System.out.println("There will be three rooms. Office, bedroom, and library where you will have to find the key to get from one room to another.");
+        //Opening prompt for escape room
+            System.out.println("Welcome to the Escape Room!");
+                    System.out.println("""
+                    There will be three rooms the bedroom, office, and bathroom. 
+                    You can start in any room but in order to go from one room to another you need to find a key in each room to unlock the door.
+                    There will clues in the room your in to find the key.""");
+                    //Ask player for name
+                    try (Scanner scan = new Scanner(System.in)) {
+                        System.out.println("What is your name?");
+                        while (scan.hasNextLine()) {
+                            String name = scan.nextLine();
+                            if (name == null || name.trim().isEmpty()) {
+                                // Error for no input, keeps asking for name 
+                                System.out.print("Please, what is your name?\n");
+                                scan.hasNextLine();
+                            } else {
+                                // Print name
+                                System.out.println("Welcome to the Escape Room " + name + " Good luck" + " !");
+                                break;
+                            } // End of conditional
+                        } // End of while loop
+                    }
     }
 }
--- Rooms.java
+++ Rooms.java
@@ -8,4 +8,33 @@ public class Rooms {
         this.description = description;
         this.objects = objects;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getobjects() {
+        return objects;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setAmount(String objects) {
+        this.objects = objects;
+    }
+
+    public static void main(String[] args) {
+        Rooms claRooms = new Rooms("DTI", "white tile floors with white painted walls", "desk");
+        System.out.println(claRooms);
+      }
 }
