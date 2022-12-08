0c0d9e822eb89435a4bb2687311dba4f12026d94
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-08 10:03:07 -0500

    Update Bathroom.java

--- Bathroom.java
+++ Bathroom.java
@@ -1,3 +1,7 @@
-public class Bathroom {
+public class Bathroom extends Rooms {
+
+    public Bathroom(String name, String description, String objects) {
+        super(name, description, objects);
+    }
     
 }
