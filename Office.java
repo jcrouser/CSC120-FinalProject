aa45faa3be8adb08c65c20c04bc289f0ca080edf
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-08 10:01:21 -0500

    Update Office.java

--- Office.java
+++ Office.java
@@ -1,3 +1,11 @@
-public class Office {
+public class Office extends Rooms {
+
+    public Office(String name, String description, String objects) {
+        super(name, description, objects);
+    }
+
     
+
+    public static void main(String[] args) {   
+    }
 }
