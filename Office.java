a567c2c18b0abf76d26eb262bcbc2b76386c6cbe
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-10 16:20:58 -0500

    Update Office.java

--- Office.java
+++ Office.java
@@ -1,44 +1,57 @@
 import java.util.Scanner;
 
-public class Office extends Rooms{
-   
-    private String desk;
+public class Office extends Rooms {
 
-
-
-    public Office (String name, String description, String objects) {
+    public Office(String name, String description, String objects) {
         super(name, description, objects);
- 
-    }
-    
-    public void desk(String objects){
-        
     }
 
+    private static void desk(){
+        System.out.println("You have looked into the drawer of the desk but there is nothing there!");
+    }
 
+    private static void chair(){
+        System.out.println("You have looked under the chair. There are the numbers 1,2,3");
+    }
 
-    
-    public static void main(String[] args) { 
-        Rooms claRooms = new Rooms("DTI", " white tile floors with white painted walls", "desk");
-        System.out.println(claRooms);
-        System.out.println("Welcome to the Office!");
-        System.out.println("Available options in the Office are: \n + desk \n + filing cabinet \n + desk chair");
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.println("Which would you like to choose?");
-            while (scan.hasNextLine()) {
-                String name = scan.nextLine();
-                if (name == null || name.trim().isEmpty()) {
-                    // Error for no input, keeps asking for name 
-                    System.out.print("Please put an option shown on the list\n");
-                    scan.hasNextLine();
-                } else {
-                    // Print name
-                    System.out.println("You have chosen " + name + "!");
+    private static void safe(){
+        System.out.println("Enter the passcode to unlock the safe and get the key!");
+    }
+
+    private static void door(){
+        System.out.println("You need a key to open the door. Open it using the key");
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What do you want to do?\n"
+        + "1. Look in the drawer of the desk\n"
+        + "2. Look under the chair\n"
+        + "3. Open the safe\n"
+        + "4. open the door\n"
+        + "5. Look through inventory\n"
+        + "6. get help\n"
+        + "7. quit game\n");
+        int answer = scan.nextInt();
+
+        while(answer > 6 || answer < 1) {
+            System.out.println("pick one of the options");
+            answer = scan.nextInt();
+        }
+        switch(answer) {
+            case 1: desk();
                     break;
-                } // End of conditional
-            } // End of while loop
+            case 2: chair();
+                    break;
+            case 3: safe();
+                    break;
+            case 4: door();
+                    break;
+         
+        }
+        scan.close();
 
-      }
+    }
+
+
 }
-    
-} 
