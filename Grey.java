c0125a7632a080611785144dc64d111f5fdb241a
alexanderyoung1020 <118072569+alexanderyoung1020@users.noreply.github.com>
2023-04-24 16:31:09 -0400

    grey cat constructor added

--- Grey.java
+++ Grey.java
@@ -1,3 +1,14 @@
-public class Grey extends Cat{
-    
+import java.util.Random;
+
+public class GreyCat extends Cat {
+
+    public GreyCat(int dexterity, int strength, int iq, int hp) {
+        super(dexterity, strength, iq, hp);
+    }
+
+    // You can add any additional methods or variables specific to the GreyCat class here
+public static void main(String[] args) {
+    GreyCat myGreyCat = new GreyCat(5, 10, 15, 9);
+
+}
 }
