2c5585382c10acc6fdebe7411234f43faf42de2d
hala <hmaloul@smith.edu>
2023-04-24 16:20:38 -0400

    made a basic consturctor

--- Cat.java
+++ Cat.java
@@ -4,6 +4,14 @@ public class Cat{
     public static int iq;
     public static int hp; 
 
+    public Cat(int dexterity,int strength,int iq, int hp){
+        this.dexterity=dexterity;
+        this.strength=strength;
+        this.iq=iq;
+        this.hp=9;
+
+    }
+
     public void hunt(){
         System.out.println("Hunt");
     }
