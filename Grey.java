f5ea4a3e5c2d35b074fce0565b92321dbd3f48ef
alexanderyoung1020 <118072569+alexanderyoung1020@users.noreply.github.com>
2023-04-24 16:32:34 -0400

    fixed grey

--- Grey.java
+++ Grey.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 
-public class GreyCat extends Cat {
+public class Grey extends Cat {
 
-    public GreyCat(int dexterity, int strength, int iq, int hp) {
+    public Grey(int dexterity, int strength, int iq, int hp) {
         super(dexterity, strength, iq, hp);
     }
 
     // You can add any additional methods or variables specific to the GreyCat class here
 public static void main(String[] args) {
-    GreyCat myGreyCat = new GreyCat(5, 10, 15, 9);
+    Grey myGreyCat = new Grey(5, 10, 15, 9);
 
 }
 }
