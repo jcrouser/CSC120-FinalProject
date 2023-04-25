daebc94f42ef78b12f0b5f27d18024a5b3b69666
Emkung <ekung@smith.edu>
2023-04-25 04:23:16 -0400

    random num added

--- Cat.java
+++ Cat.java
@@ -1,17 +1,9 @@
 import java.util.Random;
 public class Cat{
-<<<<<<< HEAD
-    public String name;
-    public int dexterity;
-    public int strength;
-    public int iq;
-    public int hp; 
-=======
     public static int dexterity;//dex is the number you add on to a long range attack, which is a random attack number from 0-20, specifically for kick 
     public static int strength;//strength is the number you add onto a attack, specifically for bite 
     public static int iq; //iq is intelligence and the ability to escape, it adds onto a random 0-20 number 
     public static int hp; //hp means the total amount of life that you have 
->>>>>>> 8bedfa36c24392bad0aeb719c27d6e318c44de8c
 
     public Cat(String name,int dexterity,int strength,int iq, int hp){
         this.dexterity=dexterity;
@@ -24,14 +16,23 @@ public class Cat{
     public void hunt(){
         System.out.println("Hunt");
     }
-    public  void kick(){
-        System.out.println("kick");
+    public  int kick(){ 
+        Random random = new Random();
+        int randomNumber = random.nextInt(21); 
+        int ttldmg = randomNumber + this.dexterity;
+        return ttldmg;
     }
-    public void bite(){
-        System.out.println("bite");
+    public int bite(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(21); 
+        int ttldmg = randomNumber + this.strength;
+        return ttldmg;
     }
-    public void escape(){
-        System.out.println("escaped!");
+    public int escape(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(21); 
+        int ttliq = randomNumber + this.iq;
+        return ttliq;
     }
     public void scratch(){
         System.out.println("scratch");
