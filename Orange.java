c89b48b02d94a971b9a6201d5dd1aed9c04b5add
Hphan777 <you@example.com>
2023-04-25 10:18:56 -0400

    Orange cat added

---
+++ Orange.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Orange extends Cat {
+
+    public Orange(int dexterity,int strength,int iq, int hp) {
+        super(dexterity, strength, iq, hp);
+    }
+
+    public static void main(String[] args) {
+        Orange myOrangeCat = new Orange(2, 20, 15, 9);
+    
+    }
+
+    
+    }
+
+
+
+
+}
+
+
