5cf5d2bca70a5e428368f6610f518a6d728a558b
Emkung <ekung@smith.edu>
2023-04-25 11:56:53 -0400

    New monster sub class

--- Monster.java
+++ Monster.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 public class Monster {
     public static String name;
-    public static int strength;//strength is the number you add onto a attack
-    public static int iq; //iq is intelligence and the ability to block escape, it adds onto a random 0-20 number 
-    public static int hp; //hp means the total amount of life that you have 
+    public static int strength = 5;//strength is the number you add onto a attack
+    public static int iq =5; //iq is intelligence and the ability to block escape, it adds onto a random 0-20 number 
+    public static int hp = 10; //hp means the total amount of life that you have 
     public Monster(String name,int strength,int iq, int hp){
         this.name = name;
         this.strength=strength;
---
+++ ParasiticMutant.java
@@ -0,0 +1,5 @@
+public class ParasiticMutant extends Monster{
+    public ParasiticMutant(String name,int strength,int iq, int hp){
+        super(name, 8, iq, hp);
+    }
+}
