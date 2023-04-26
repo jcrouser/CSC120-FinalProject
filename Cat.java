976642d1e63625617e24ba19595f64cf7d648764
hala <hmaloul@smith.edu>
2023-04-26 19:44:38 -0400

    java doc cat class

--- Cat.java
+++ Cat.java
@@ -1,4 +1,8 @@
 import java.util.Random;
+/**
+
+The Cat class represents a cat in the game.
+*/
 public class Cat{
     public static String name;
     public static int dexterity = 5;//dex is the number you add on to a long range attack, which is a random attack number from 0-20, specifically for kick 
@@ -6,6 +10,15 @@ public class Cat{
     public static int iq = 5; //iq is intelligence and the ability to escape, it adds onto a random 0-20 number 
     public static int hp = 9; //hp means the total amount of life that you have 
     public static Gameplay gameplay;
+    /**
+    * Constructor for the Cat class.
+    * @param name The name of the cat.
+    * @param dexterity The dexterity of the cat.
+    * @param strength The strength of the cat.
+    * @param iq The intelligence of the cat.
+    * @param hp The hit points of the cat.
+    * @param gameplay The gameplay object that the cat interacts with.
+    */
     public Cat(String name,int dexterity,int strength,int iq, int hp, Gameplay gameplay){
         this.name = name;
         this.dexterity = dexterity;
@@ -14,22 +27,37 @@ public class Cat{
         this.hp = hp;
         this.gameplay = gameplay;
     }
-
+    /**
+    * we don't know yet.
+    */
     public void hunt(){
         System.out.println("Hunt");
     }
+
+    /**
+    * The cat kicks
+    * @return The total damage dealt by the cat's kick.
+    */
     public int kick(){ 
         Random random = new Random();
         int randomNumber = random.nextInt(21); 
         int ttl_dmg = randomNumber + this.dexterity;
         return ttl_dmg;
     }
+    /**
+     * The cat bites.
+    * @return The total damage dealt by the cat's bite.
+    */
     public int bite(){
         Random random = new Random();
         int randomNumber = random.nextInt(21); 
         int ttl_dmg = randomNumber + this.strength;
         return ttl_dmg;
     }
+    /**
+    * The cat tries to escape.
+    * @return The total IQ of the cat and a random number from 0-20.
+    */
     public int escape(){
         Random random = new Random();
         int randomNumber = random.nextInt(21); 
@@ -45,9 +73,17 @@ public class Cat{
     public  void purr(){
         System.out.println("The cats collectively purrs");
     }
+    /**
+    * Gets the name of the cat.
+    * @return The name of the cat.
+    */
     public String getName(){
         return this.name;
     }
+    /**
+    * Gets the dexterity of the cat.
+    * @return The dexterity of the cat.
+    */
     public int getDexterity(){
         return this.dexterity;
     }
